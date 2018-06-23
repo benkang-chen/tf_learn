6f98017d02c26dfde764fec9e1f8d3d05f07ac17
benkang.chen <benkang.chen@baifendian.com>
2018-06-24 01:36:12 +0800

    入门

---
+++ brief_introduction/brief_introduction.py
@@ -0,0 +1,35 @@
+"""
+这段很短的 Python 程序生成了一些三维数据, 然后用一个平面拟合它.
+"""
+import tensorflow as tf
+import numpy as np
+
+# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
+x_data = np.float32(np.random.rand(2, 100)) # 2 X 100 维的数组
+y_data = np.dot([0.100, 0.200], x_data) + 0.300 # 1 X 100维的数组
+
+
+# 构造一个线性模型
+b = tf.Variable(tf.zeros(1))
+W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
+y = tf.matmul(W, x_data) + b
+
+# 最小化方差
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(loss)
+train = optimizer.minimize(loss)
+
+# 初始化变量
+init = tf.global_variables_initializer()
+
+# 启动图
+sess = tf.Session()
+sess.run(init)
+
+# 拟合平面
+for step in range(0, 201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, "---", sess.run(W), sess.run(b))
+
+
